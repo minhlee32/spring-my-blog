ca656117d90659efb3f0497d4efc5644aca25301
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-23 21:01:40 +0700

    create interface service for post

---
+++ src/main/java/com/codegym/service/PostService.java
@@ -0,0 +1,16 @@
+package com.codegym.service;
+
+import com.codegym.model.Post;
+
+import java.util.List;
+
+public interface PostService {
+
+    List<Post> findAll();
+
+    Post findById(Long id);
+
+    void save(Post post);
+
+    void remove(Long id);
+}
