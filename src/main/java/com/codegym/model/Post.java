6260bd6a36915e220767d3c0573334a9c636a062
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-23 20:49:36 +0700

    create model for post

---
+++ src/main/java/com/codegym/model/Post.java
@@ -0,0 +1,59 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @Lob
+    private String context;
+
+    public Post() {
+    }
+
+    public Post(String title, String description, String context) {
+        this.title = title;
+        this.description = description;
+        this.context = context;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+}
