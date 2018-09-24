ce4ceb1d4dd34a40610e8f45325210143d01eaa8
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-24 08:39:35 +0700

    extends interface paging and sorting repository

--- src/main/java/com/codegym/repository/PostRepository.java
+++ src/main/java/com/codegym/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.codegym.repository;
 
 import com.codegym.model.Post;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PostRepository extends Repository<Post> {
+public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
 }
